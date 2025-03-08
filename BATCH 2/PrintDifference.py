d85bd846494a0def1a3dab8c9535ea91de1cc803
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 17:07:13 +0800

    Update and rename PROG03.py to PrintDifference.py

--- BATCH 2/PrintDifference.py
+++ BATCH 2/PrintDifference.py
@@ -1,6 +1,6 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
-e=int(input("Enter first number: "))
-f=int(input("Enter second number: "))
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
 
-print(e-f)
+print(num1-num2)
