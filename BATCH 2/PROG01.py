e0af4b9e7485450ed088d0ca9a939e8bfb6a9bb6
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 17:05:46 +0800

    Update and rename PROG01.py to PrintSmallNum.py

--- BATCH 2/PROG01.py
+++
@@ -1,9 +0,0 @@
-#Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
-
-a= int(input("Enter first number: "))
-b= int(input("Enter second number: "))
-
-if a<b:
-    print(a)
-else:
-    print (b)
