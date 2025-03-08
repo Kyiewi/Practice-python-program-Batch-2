3abc6c95c2520c38c7611d415a23dd157bd3ecf9
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 17:06:32 +0800

    Update and rename PROG02.py to PrintNotEqual.py

--- BATCH 2/PrintNotEqual.py
+++ BATCH 2/PrintNotEqual.py
@@ -1,8 +1,8 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
-c=int(input("Enter first number: "))
-d=int(input("Enter second number: "))
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
 
-if c!=d:
+if num1!=num2:
     print("Not Equal")
 
