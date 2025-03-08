d9d9211678e13f6d282f3c50f2bbbf3ed52e0050
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 17:28:59 +0800

    Update and rename PROG08.py to PrintAllOddnumFrom0to100.py

--- BATCH 2/PrintAllOddnumFrom0to100.py
+++ BATCH 2/PrintAllOddnumFrom0to100.py
@@ -1,7 +1,7 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-noomber = 1
+number = 1
 
-while noomber <= 100:
-    print(noomber)  
-    noomber += 2
+while number <= 100:
+    print(number)  
+    number += 2
