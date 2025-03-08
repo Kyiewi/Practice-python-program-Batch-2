883278ecd6776df52dc591df785653fe3c4d22bf
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 17:10:42 +0800

    Update and rename PROG04.py to PrintQuotientNoDeci.py

--- BATCH 2/PrintQuotientNoDeci.py
+++ BATCH 2/PrintQuotientNoDeci.py
@@ -1,6 +1,6 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
-g=float(input("Enter first number: "))
-h=float(input("Enter second number: "))
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
 
-print (g//h)
+print (num1//num2)
