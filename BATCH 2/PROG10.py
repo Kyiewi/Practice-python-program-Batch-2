df5a565f8e01e9a5098452635e532d155ffea7f9
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 17:26:49 +0800

    Update and rename PROG10.py to PrintAllNumBetween2Num.py

--- BATCH 2/PROG10.py
+++
@@ -1,7 +0,0 @@
-#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
-
-y= int(input("Enter first number: "))
-z= int(input("Enter second number: "))
-
-for uiaaiuiiai in range(y + 1, z):
- print(uiaaiuiiai)
---
+++ BATCH 2/PrintAllNumBetween2Num.py
@@ -0,0 +1,7 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+
+for num in range(y + 1, z):
+ print(num)
