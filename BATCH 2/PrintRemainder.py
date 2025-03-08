6501febc7d412a26ac033ef30a42a8874bdd6391
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-08 17:17:24 +0800

    Update and rename PROG05.py to PrintRemainder.py

--- BATCH 2/PrintRemainder.py
+++ BATCH 2/PrintRemainder.py
@@ -1,5 +1,5 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
-i=int(input("Enter first number: "))
-j=int(input("Enter second number: "))
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
 
-print (i%j)
+print (num1%num2)
